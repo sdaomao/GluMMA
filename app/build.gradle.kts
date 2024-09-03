1e0b305833e87ab89bd65f5c8070bb0dc78aaa90
sadao <ezzyy18@gmail.com>
2024-09-03 04:16:36 +0100

    adjustments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
